ce77a3032ff75d2a370d36d9ba3ae5ddbdc0f4fe
shaca81 <stankovic.milos@gmail.com>
2016-10-11 10:38:10 +0400

    Create automategui.py

---
+++ automategui.py
@@ -0,0 +1,22 @@
+"""
+Some basic automation of mouse and keyboard
+"""
+
+import pyautogui
+import time
+
+print(pyautogui.size())  # information about the screen size
+print(pyautogui.position())  # information about the current position of the mouse pointer
+
+# Section to open the browser, open the new tab and go to google website
+pyautogui.click(77, 1066, button='left', duration=0.25)
+time.sleep(5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(2)
+pyautogui.typewrite('http://www.google.com\n')
+
+
+# Some other code not used
+#pyautogui.click(10, 183, button='left', duration=0.1)
+
+#pyautogui.click(555, 270); pyautogui.typewrite('Hello world!\n')
