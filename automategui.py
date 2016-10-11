f674500f1f9230ddd350684f57b184fa1e6124af
shaca81 <stankovic.milos@gmail.com>
2016-10-11 10:39:26 +0400

    Update automategui.py

--- automategui.py
+++ automategui.py
@@ -8,12 +8,12 @@ import time
 print(pyautogui.size())  # information about the screen size
 print(pyautogui.position())  # information about the current position of the mouse pointer
 
-# Section to open the browser, open the new tab and go to google website
+# Section to open the browser, open the new tab and go to github website
 pyautogui.click(77, 1066, button='left', duration=0.25)
 time.sleep(5)
 pyautogui.hotkey('ctrl', 't')
 time.sleep(2)
-pyautogui.typewrite('http://www.google.com\n')
+pyautogui.typewrite('http://www.github.com\n')
 
 
 # Some other code not used
